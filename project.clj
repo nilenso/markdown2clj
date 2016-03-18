30509030a772dc7018773eb51eaf008c45748649
Unnikrishnan <krish8664@gmail.com>
2016-03-18 17:06:55 +0530

    Update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject markdown2clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject markdown2clj "0.1.0"
+  :description "A librabry to convert markdown string to clojure data-structure"
+  :url "http://nilenso.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
